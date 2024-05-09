646acf28f25293986b53f5f79be1853f52dc4c94
HP <wailmessaoudi806@gmail.com>
2024-05-09 22:43:02 +0100

    i added the comboboxes

--- app.py
+++ app.py
@@ -1,100 +1,125 @@
 import tkinter as tk
+from tkinter import ttk
 import osmnx as ox
 from utilities import astar_search, euclidean_distance
 
-def show_screen2(screen1, screen2):
-  # Show screen2
-  screen2.pack(fill=tk.BOTH, expand=True)
-  # Hide current screen (screen1)
-  screen1.pack_forget()
 
-def show_screen3(screen2, screen3):
-  screen2.pack_forget()
-  # Show screen3
-  screen3.pack(fill=tk.BOTH, expand=True)
+
+
+
+def get_state_id(state_name):
+
+  state_ids = {
+    "Adrar": 1,
+    "Chlef": 2,
+    "Guelma": 3,
+    "Oum El Bouaghi": 4,
+    "Souk Ahras": 5,
+    "Batna": 6,
+    "Djelfa": 7,
+    "Illizi": 8,
+    "Oran": 9,
+    "Tamanrasset": 10,
+    "Béchar": 11,
+    "El Bayadh": 12,
+    "Khenchela": 13,
+    "Ouargla": 14,
+    "Tindouf": 15,
+    "Biskra": 16,
+    "El Oued": 17,
+    "Laghouat": 18,
+    "Sétif": 19,
+    "Tissemsilt": 20,
+    "Bordj Bou Arreridj": 21,
+    "Médéa": 22,
+    "M'Sila": 23,
+    "Tiaret": 24,
+    "Tlemcen": 25,
+    "Bouira": 26,
+    "Mila": 27,
+    "Naâma": 28,
+    "Tizi Ouzou": 29,
+    "Constantine": 30,
+    "Mostaganem": 31,
+    "Saïda": 32,
+    "Skikda": 33,
+    "Tébessa": 34
+}
+
+  if state_name in state_ids:
+    return state_ids[state_name]
+  else:
+    return None
+
+def main_code(start, destination):
+  # ox.config(use_cache=True, log_console=True)
+
+  # # Define the location (for example, a city name or address)
+  # place_name = "New York City, USA"
+
+  # # Download the street network for the specified location
+  # G = ox.graph_from_place(place_name, network_type='drive')
+  # G = ox.add_edge_speeds(G)
+  # G = ox.add_edge_travel_times(G)
+
+  # number_of_nodes = G.number_of_nodes()
+  # print(f"number of nodes is {number_of_nodes}")
+
+  # # Plot the graph
+  # ox.plot_graph(G)
+
+  start_node = get_state_id(start)
+  goal_node = get_state_id(destination)
+
+  print(start)
+  print(destination)
+
+  # shortest_path = astar_search(G, start_node, goal_node, euclidean_distance)
+  # print(shortest_path)
 
 def create_app_ui():
-    root = tk.Tk()
 
-    root.geometry("500x500")
-    root.title("shortest-path")
+  states = [
+      "Adrar", "Chlef", "Guelma", "Oum El Bouaghi", "Souk Ahras",
+      "Batna", "Djelfa", "Illizi", "Oran", "Tamanrasset",
+      "Béchar", "El Bayadh", "Khenchela", "Ouargla", "Tindouf",
+      "Biskra", "El Oued", "Laghouat", "Sétif", "Tissemsilt",
+      "Bordj Bou Arreridj", "Médéa", "M'Sila", "Tiaret", "Tlemcen",
+      "Bouira", "Mila", "Naâma", "Tizi Ouzou", 
+      "Constantine", "Mostaganem", "Saïda",
+      "Djelfa", "Skikda", "Tébessa"
+  ]
 
-    # Define frames for each screen
-    screen1 = tk.Frame(root)
-    screen2 = tk.Frame(root)
-    screen3 = tk.Frame(root)
+  root = tk.Tk()
 
-    # Screen 1
-    # Outer frame (fills the window)
-    outer_frame = tk.Frame(screen1)
-    outer_frame.pack(fill=tk.BOTH, expand=True)
+  root.geometry("500x500")
+  root.title("shortest-path")
 
-    # Inner frame (holds label and button)
-    inner_frame = tk.Frame(outer_frame)
-    inner_frame.pack()
+  # Main frame
+  main_frame = tk.Frame(root)
+  main_frame.pack(fill=tk.BOTH, expand=True)
 
-    label = tk.Label(inner_frame, text= "Welcome shortest-path", font=('Arial', 18))
-    label.pack()
+  # ComboBox Row
+  combobox_row = tk.Frame(main_frame)
 
-    button = tk.Button(inner_frame, text="Next", font=('Arial', 10), command = show_screen2(screen1, screen2))
-    button.pack()
+  # Create the combo box
+  start_combo_box = ttk.Combobox(combobox_row, values=states)
+  start_combo_box.current(0)
+  destenation_combo_box = ttk.Combobox(combobox_row, values=states)
+  destenation_combo_box.current(0)
 
-    # Screen 2
-    # Outer frame (fills the window)
-    outer_frame = tk.Frame(screen2)
-    outer_frame.pack(fill=tk.BOTH, expand=True)
+  # Widgets
+  label1 = tk.Label(main_frame, text= "Welcome shortest-path", font=('Roboto', 18))
+  label2 = tk.Label(main_frame, text= "Please choose the starting city and the destination city", font=('Roboto', 12))
+  button = tk.Button(main_frame, text="Generate", font=('Roboto', 10), command= main_code(start_combo_box.get(), destenation_combo_box.get()))
 
-    # Inner frame (holds label and button)
-    inner_frame = tk.Frame(outer_frame)
-    inner_frame.pack()
+  label1.pack(pady=20)
+  label2.pack(pady=10)
+  combobox_row.pack(fill=tk.X, pady=10)
+  start_combo_box.pack(side= "left", padx=10)
+  destenation_combo_box.pack(side= "right", padx=10)
+  button.pack(pady=10)
 
-    label = tk.Label(inner_frame, text= "Choose the starting point", font=('Arial', 18))
-    label.pack()
+  root.mainloop()
 
-    button = tk.Button(inner_frame, text="Algiers", font=('Arial', 10), command = show_screen3(screen1, screen2, screen3))
-    button.pack()
-
-    # Screen 3
-    # Outer frame (fills the window)
-    outer_frame = tk.Frame(screen3)
-    outer_frame.pack(fill=tk.BOTH, expand=True)
-
-    # Inner frame (holds label and button)
-    inner_frame = tk.Frame(outer_frame)
-    inner_frame.pack()
-
-    label = tk.Label(inner_frame, text= "Choose the goal point", font=('Arial', 18))
-    label.pack()
-
-    button = tk.Button(inner_frame, text="Algiers", font=('Arial', 10))
-    button.pack()
-
-    screen1.pack(fill=tk.BOTH, expand=True)
-    screen2.pack_forget()
-    screen3.pack_forget()
-
-    root.mainloop()
-
-create_app_ui()
-
-ox.config(use_cache=True, log_console=True)
-
-# Define the location (for example, a city name or address)
-place_name = "New York City, USA"
-
-# Download the street network for the specified location
-G = ox.graph_from_place(place_name, network_type='drive')
-G = ox.add_edge_speeds(G)
-G = ox.add_edge_travel_times(G)
-
-number_of_nodes = G.number_of_nodes()
-print(f"number of nodes is {number_of_nodes}")
-
-# Plot the graph
-ox.plot_graph(G)
-
-start_node = 9498751775
-goal_node = 11890331361
-
-shortest_path = astar_search(G, start_node, goal_node, euclidean_distance)
-print(shortest_path)
+create_app_ui
